e1e8835a93ec64ed32f534ed302b6f1095c6ca96
Jose Machicado <josemiguelmc_1998@hotmail.com>
2021-11-27 11:42:57 -0300

    fusion commits subida imagenes

--- index.js
+++ index.js
@@ -11,19 +11,31 @@ require('dotenv').config(); // para que se pueda usar variables de entorno esto 
 const publicPaths = require('./config/public-paths'); //aca definiremos que rutas seran publicas ...o sea para q todo mundo pueda obtener los datos de las rutas
 const extractJwt = require('@utils/extract-jwt'); //esto es para decodear o parsear el token que deberia llegar
 const app = express();
+const path = require('path');
 const cors = require('cors'); //esto para q acepte peticiones de otros servidores en este caso nuestro angular localhost:4200
 app.use(express.json()); //para que express entienda objetos json
 app.use(express.urlencoded({extended:false})); // para que entienda lo que viene de formulario html
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev')); //configuracion para ver las peticiones que se le hacen a la api y el estado q devuelve
 // instale el nodemon esto es una maravilla xD basicamente sin esto tendriamos q detener la api y volver a correr por cada "guardar" que hagamos 
 // y nodemon te lo hace automaticamente ver package.json ("start": "nodemon index.js" esto en produccion no iria ya que no sera "correcto" pero a nosotros nos re sirve para hacernos la vida mas facil) 
 global.fetch = require('node-fetch');
+const formData = require('express-form-data');
 const mongoose = require('mongoose'); //mongoose es mongo basicamente ...a continuacion nos conectaremos a la base de datos 
-mongoose.Promise = Promise; // proccess.env es como indicaremos que nos referimos a la variable de entorno
+mongoose.Promise = Promise;
+
+const options = {
+    uploadDir: process.env.UPLOAD_FILE_TMP_FOLDER,
+    autoClean: true
+}; // proccess.env es como indicaremos que nos referimos a la variable de entorno
 return mongoose.connect('mongodb://' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DB,
 {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
+    app.use(formData.parse(options));
+    app.use(formData.format());
+    app.use(formData.union());
+
     app.get(publicPaths.regex('get'), extractJwt);
     app.put(publicPaths.regex('put'), extractJwt);
     app.post(publicPaths.regex('post'), extractJwt);
